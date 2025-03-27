93b4e50d55194e4448a3d25ab2bfb4a6804bf07e
James-T-Ardian <jamesardian01@gmail.com>
2025-03-26 19:27:17 -0700

    use hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import SearchPage from "./SearchPage";
 import WorkInProgress from "./WorkInProgress";
 import Mattress from "./Mattress";
